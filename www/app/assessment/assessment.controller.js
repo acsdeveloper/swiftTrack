cd9dafed641bed56111c27822f3b296c878528ba
acsdeveloper <developer@agilexcyber.in>
2018-05-17 13:19:22 +0530

    assessment changes-video image

--- www/app/assessment/assessment.controller.js
+++ www/app/assessment/assessment.controller.js
@@ -1,5 +1,5 @@
 (function() {
-    // 'use strictl';
+    // 'use strict ck';
     function assessmentCtrl(pdf,$sce,$state, $ionicModal, $scope, $http, $location, $cookieStore, storageFactory, ModuleService) {
         console.log("after ctrl");
         var vm = this;
