2a55ed0072bf5151c5b7a95d914a511d71ba0269
acsdeveloper <developer@agilexcyber.in>
2018-05-17 13:12:55 +0530

    test changes

--- www/app/assessment/assessment.controller.js
+++ www/app/assessment/assessment.controller.js
@@ -1,5 +1,5 @@
 (function() {
-    // 'use strict';
+    // 'use strictl';
     function assessmentCtrl(pdf,$sce,$state, $ionicModal, $scope, $http, $location, $cookieStore, storageFactory, ModuleService) {
         console.log("after ctrl");
         var vm = this;
